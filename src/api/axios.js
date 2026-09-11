a9493ce8075706694dc05442b3fdd2853cae1e6e
sunkanmi <iolasunkanmi405@gmail.com>
2026-09-11 05:08:50 +0100

    changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // your backend's address
+  baseURL: " https://vendorflow-backend-6nuy.onrender.com/api",
 });
 
-// This runs before every request — it grabs the saved token and attaches it
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
