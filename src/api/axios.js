b362600e292a6696f3ab5b5e693bd8dbe030165e
sunkanmi <iolasunkanmi405@gmail.com>
2026-09-14 20:03:59 +0100

    changedd

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://vendorflow-backend-6nuy.onrender.com/api",
+  baseURL: "https://vendorflow-backend-6nuy.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
